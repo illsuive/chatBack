63b020b8259b3e22c459e934f21d654046fc933e
Gautam <gautambutola@outlook.com>
2026-05-18 00:39:02 +0530

    Add success message on successful login

--- src/mvc/controller/auth.controller.js
+++ src/mvc/controller/auth.controller.js
@@ -57,6 +57,7 @@ export const login = async (req, res) => {
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             secure: process.env.NODE_ENV === 'production'
         });
+        
         res.status(200).json({ message: 'Logged in successfully', success: true, user: userWithoutPassword })
     } catch (error) {
         res.status(500).json({ message: error.message, success: false })
